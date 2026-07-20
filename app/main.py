99e22d02cd1ff2f9a83284f8190eca6c1a8b9332
Saswat <mohantysaswat154@gmail.com>
2026-07-20 10:13:39 +0530

    Model Fields using pydantic

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI, HTTPException, status # type: ignore
 from scalar_fastapi import get_scalar_api_reference # type: ignore
 from typing import Any
-from pydantic import BaseModel
 
-class Shipment(BaseModel):
-    content:str
-    weight: float
-    destination: int
+from app.schemas import Shipment
 
 app = FastAPI()
 
@@ -71,12 +67,6 @@ def get_shipment_by_id(id: int) -> dict[str, Any]:
 
 @app.post("/shipment")
 def submit_shipment(shipment: Shipment) -> dict[str, Any]:
-    # Validate weight:
-    if shipment.weight > 25:
-        raise HTTPException(
-            status_code = status.HTTP_406_NOT_ACCEPTABLE,
-            detail = "Maximum weight limit is 25kg"
-        )
     # Create and assignshipment a new id
     new_id = max(shipments.keys()) + 1
     shipments[new_id] = {
