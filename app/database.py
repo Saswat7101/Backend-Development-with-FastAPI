c5b8d6ac03481fb6ddd3d914166f565597f0291a
Saswat <mohantysaswat154@gmail.com>
2026-07-21 10:50:39 +0530

    Adding PRIMARY KEY to table

--- app/database.py
+++ app/database.py
@@ -6,19 +6,26 @@ cursor = connection.cursor()
 
 # Create a table
 cursor.execute(
-    "CREATE TABLE IF NOT EXISTS shipment (id INTEGER, content TEXT, weight REAL, status TEXT)"
+    "CREATE TABLE IF NOT EXISTS shipment (id INTEGER PRIMARY KEY, content TEXT, weight REAL, status TEXT)"
 )
 
+# Drop a table
+# cursor.execute("DROP TABLE shipment")
+
 # Add shipment data
-# cursor.execute(
-#     "INSERT INTO shipment VALUES (12702, 'basalt', 18.7, 'In Transit)"
-# )
-# cursor.commit()
+cursor.execute(
+    "INSERT INTO shipment (id, content, weight, status) VALUES (12701, 'Wooden Table', 2.5, 'In Transit'), (12702, 'Ceramic Mugs', 0.8, 'Placed'), (12703, 'Office Chair', 14.2, 'Out for Delivery'), (12704, 'Wireless Headphones', 1.1, 'Delivered'), (12705, 'Kitchen Blender', 6.7, 'Processing'), (12706, 'Cotton Bed Sheets', 3.4, 'In Transit'), (12707, 'Floor Lamp', 9.5, 'Delayed'), (12708, 'Palm Trees', 8.0, 'Placed');"
+)
+connection.commit()
 
 # Read a shipment by id
 cursor.execute("SELECT * FROM shipment WHERE id = 12701")
 result = cursor.fetchall()
 print(result)
+
+# Delete a record by id
+# cursor.execute("DELETE FROM shipment WHERE id = 12703")
+# connection.commit()
 
 # Close the connection once db operations are done
 connection.close()
