7c083f8ec419f33d845abc10fe2b411a16d83bf8
Saswat <mohantysaswat154@gmail.com>
2026-07-23 10:33:52 +0530

    Dependency Injection

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, HTTPException, status  # type: ignore
-from scalar_fastapi import get_scalar_api_reference  # type: ignore
+from fastapi import Depends, FastAPI, HTTPException, status  # type: ignore
+from scalar_fastapi import get_scalar_api_reference
+from sqlmodel import Session  # type: ignore
 
-from app.database.session import create_db_tables
+from app.database.models import Shipment
+from app.database.session import create_db_tables, get_session
 
 from .schemas import ShipmentRead, ShipmentCreate, ShipmentUpdate
 from .database import Database
@@ -21,8 +23,8 @@ db = Database()
 
 # Read a shipment by id
 @app.get("/shipment", response_model=ShipmentRead)
-def get_shipment_by_id(id: int):
-    shipment = db.get(id)
+def get_shipment_by_id(id: int, session: Session = Depends(get_session)):
+    shipment = session.get(Shipment, id)
     # Check for shipment with given id
     if shipment is None:
         raise HTTPException(
@@ -34,7 +36,9 @@ def get_shipment_by_id(id: int):
 
 # Create a new shipment with content and weight
 @app.post("/shipment", response_model=None)
-def submit_shipment(shipment: ShipmentCreate) -> dict[str, int]:
+def submit_shipment(
+    shipment: ShipmentCreate, session: Session = Depends(get_session)
+) -> dict[str, int]:
     new_id = db.create(shipment)
     # Return the response
     return {"id": new_id}
@@ -42,7 +46,9 @@ def submit_shipment(shipment: ShipmentCreate) -> dict[str, int]:
 
 # Update fields of a shipment
 @app.patch("/shipment", response_model=ShipmentRead)
-def update_shipment(id: int, shipment: ShipmentUpdate):
+def update_shipment(
+    id: int, shipment: ShipmentUpdate, session: Session = Depends(get_session)
+):
     # Update the provided fields
     shipment = db.update(id, shipment)
     return shipment
@@ -50,7 +56,7 @@ def update_shipment(id: int, shipment: ShipmentUpdate):
 
 # Delete a shipment by id
 @app.delete("/shipment")
-def delete_shipment(id: int) -> dict[str, str]:
+def delete_shipment(id: int, session: Session = Depends(get_session)) -> dict[str, str]:
     db.delete(id)
     return {"detail": f"Shipment with #{id} id deleted!"}
 
